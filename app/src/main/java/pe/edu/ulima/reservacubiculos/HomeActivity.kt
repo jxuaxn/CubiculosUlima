29f71bfd17d9ac8897b14afa3d1e7425a353a5e6
jxuaxn <20153324@aloe.ulima.edu.pe>
2020-07-12 02:36:41 -0500

    agrega toolbar y pagina de bug

--- app/src/main/java/pe/edu/ulima/reservacubiculos/HomeActivity.kt
+++ app/src/main/java/pe/edu/ulima/reservacubiculos/HomeActivity.kt
@@ -1,7 +1,12 @@
 package pe.edu.ulima.reservacubiculos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_home.*
 
 
 class HomeActivity : AppCompatActivity() {
@@ -10,10 +15,27 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         val bundle : Bundle? = intent.extras
         val email:String? = bundle?.getString("email")
-
         setup()
+        setSupportActionBar(toolbar)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.bug){
+            val homeIntent = Intent(this, BugActivity::class.java).apply {
+            }
+            startActivity(homeIntent)
+        }
+        return false
     }
     private fun setup(){
-        title = "Reserva de Cubículos"
+
     }
 }
