cd9d047e6afe36d54576436ceecb2f4b198b9bdd
jxuaxn <20153324@aloe.ulima.edu.pe>
2020-07-12 02:38:20 -0500

    .

---
+++ app/src/main/java/pe/edu/ulima/reservacubiculos/BugActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.ulima.reservacubiculos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BugActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bug)
+    }
+}
